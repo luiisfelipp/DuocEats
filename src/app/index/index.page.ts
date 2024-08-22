0386b2bc41b6aa15cb7570f646515386ad2eff81
Luis <luis.felipmg@gmail.com~>
2024-08-22 16:29:03 -0400

    Se modificaron los botones

--- src/app/index/index.page.ts
+++ src/app/index/index.page.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 export class IndexPage {
   items = [
     { name: 'Paradiso', imgSrc: 'assets/icon/icon1.jpg', link: '/page1' },
-    { name: 'Starbucks', imgSrc: 'assets/icon/icon2.jpg', link: '/page2' },
+    { name: 'MAXIK', imgSrc: 'assets/icon/maxik.jpeg', link: '/page2' },
     { name: 'Casino', imgSrc: 'assets/icon/icon3.png', link: '/page3' },
     { name: 'Pizzeria', imgSrc: 'assets/icon/icon4.jpg', link: '/page4' },
   ];
