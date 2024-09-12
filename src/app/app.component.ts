9b633d273b39086e001fa8f0d39bc83b71c580ea
kriskross <cr.ramirezu@duocuc.cl>
2024-09-12 17:19:09 -0300

    ultima version navfutter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  showFooter: boolean = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const currentRoute = event.urlAfterRedirects;
+        // Oculta el footer en login y register
+        this.showFooter = !(currentRoute === '/login' || currentRoute === '/register');
+      }
+    });
+  }
+
+  navigateTo(url: string) {
+    this.router.navigate([url]);
+  }
 }
--- src/app/usuarios/mi-perfil/mi-perfil.page.ts
+++ src/app/usuarios/mi-perfil/mi-perfil.page.ts
@@ -10,8 +10,23 @@ export class MiPerfilPage implements OnInit {
 
   constructor(private router: Router) { }
 
+  // Función para navegar a cualquier página
   navigateTo(link: string) {
     this.router.navigate([link]);
+  }
+
+  // Función para ir a la página de admin
+  navigateToAdmin() {
+    this.router.navigate(['/admin']);
+  }
+
+  // Función para cerrar sesión
+  cerrarSesion() {
+    // Aquí pones la lógica para cerrar sesión, como limpiar tokens, remover datos de sesión, etc.
+    console.log('Sesión cerrada');
+
+    // Redirige al usuario a la página de inicio de sesión
+    this.router.navigate(['/login']);
   }
 
   ngOnInit() {
