c652d03c5d6fcccb4ca9530b345b4417a7a96f99
Luis <luis.felipmg@gmail.com~>
2024-10-08 15:58:31 -0400

    Implementado Loading en login

--- src/app/seguridad/login/login.page.ts
+++ src/app/seguridad/login/login.page.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/models/user.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-login',
@@ -17,15 +18,34 @@ export class LoginPage implements OnInit {
   })
 
   firebaseSvc = inject(FirebaseService);
+  utilSvc = inject(UtilsService);
 
   ngOnInit() {
     
   }
 
-  submit(){
+  async submit(){
     if (this.form.valid){
+
+      const loading = await this.utilSvc.loading();
+      await loading.present();
+
       this.firebaseSvc.signIn(this.form.value as User).then(res => {
         console.log(res);
+
+      }).catch(error => {
+        console.log(error);
+
+        this.utilSvc.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'primary',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        })
+
+      }).finally(() => {
+        loading.dismiss();
       })
     }
   }
